719623a9247fdc188b05f95ed1a6053e4d3ed028
lucasandrad <lucasandrad@yandex.com>
2018-04-24 23:12:47 -0300

    Created Dependente class

---
+++ das/calculoDeIRPF/calculoDeIRPF/Dependente.java
@@ -0,0 +1,23 @@
+public class Dependente {
+	double deducao = 189.58;
+	String nome;
+	String sexo;
+	public double getDeducao() {
+		return deducao;
+	}
+	public void setDeducao(double deducao) {
+		this.deducao = deducao;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+}
