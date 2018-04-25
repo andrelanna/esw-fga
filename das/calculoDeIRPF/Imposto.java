b31d7aaa1556882e7feba7e8744c8cd5842bebb3
lucasandrad <lucasandrad@yandex.com>
2018-04-24 22:17:29 -0300

    Created Imposto class, with aliquota, baseCalculo and valorImposto attributes

---
+++ das/calculoDeIRPF/Imposto.java
@@ -0,0 +1,27 @@
+
+public class Imposto {
+	float aliquota;
+	float baseCalculo;
+	float valorImposto;
+	
+	public float getAliquota() {
+		return aliquota;
+	}
+	public void setAliquota(float aliquota) {
+		this.aliquota = aliquota;
+	}
+	public float getBaseCalculo() {
+		return baseCalculo;
+	}
+	public void setBaseCalculo(float baseCalculo) {
+		this.baseCalculo = baseCalculo;
+	}
+	public float getValorImposto() {
+		return valorImposto;
+	}
+	public void setValorImposto(float valorImposto) {
+		this.valorImposto = valorImposto;
+	}
+	
+	
+}
